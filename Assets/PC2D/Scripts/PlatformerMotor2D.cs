701504b61b9e64e5e85ef3fd0d8b448d4f31b714
龙门特产龙井茶 <tieyewang054@gmail.com>
2025-08-03 21:42:41 +0800

    update: 更新技能逻辑

--- Assets/PC2D/Scripts/PlatformerMotor2D.cs
+++ Assets/PC2D/Scripts/PlatformerMotor2D.cs
@@ -860,19 +860,31 @@ public class PlatformerMotor2D : MonoBehaviour
     }
     public void HandleBuild()
     {
-        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, Mathf.Infinity, checkMask);
-        if (hit.collider != null && ((this.transform.position.y - hit.transform.position.y) > 2.0F))
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, 1.0f, checkMask);
+
+        if (hit.collider == null)
+        {
+            /*Debug.Log("Can Build");*/
+            SpecialManager.instance.BuildBox(this.transform.position + new Vector3(0,-1.0f,0));
+        }
+        
+        /*if (hit.collider != null && ((this.transform.position.y - hit.transform.position.y) > 1.0F))
         {
             /*
             Debug.DrawRay(this.transform.position, Vector2.down * 10f, Color.red, 1f);
             Debug.Log(hit.transform.position);
-            */
+            #1#
             SpecialManager.instance.BuildBox(this.transform.position + new Vector3(0,hit.transform.position.y-this.transform.position.y+0.5F,0));
             /*GameObject go = Instantiate(BoxsPrefab, hit.transform.position+new Vector3(0,0.5f,0), Quaternion.identity);
             SpecialBox sb = go.GetComponent<SpecialBox>();
             sb.sprite = BoxSprites[(int)SeasonManager.Instance.CurrentSeason];
-            BoxsBuildTime.Add(sb);*/
+            BoxsBuildTime.Add(sb);#1#
         }
+
+        if (hit.collider != null)
+        {
+            Debug.Log((this.transform.position.y - hit.transform.position.y));
+        }*/
     }
 
     private void HandleWallInteraction()
